d3353435a78a504c6d619c0ea4fe0fbfd316ffe1
rianmfir <rianmfirdaus@gmail.com>
2019-09-21 07:06:31 +0700

    Add ProfileActivity Memanggil Activity dari fragment

--- app/src/main/java/com/example/belajarfragment/DetailCategoryFragment.java
+++ app/src/main/java/com/example/belajarfragment/DetailCategoryFragment.java
@@ -1,6 +1,7 @@
 package com.example.belajarfragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -81,6 +82,8 @@ public class DetailCategoryFragment extends Fragment implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.btn_profile:
+                Intent mIntent = new Intent(getActivity(), ProfileActivity.class);
+                startActivity(mIntent);
                 break;
 
             case R.id.btn_show_dialog:
